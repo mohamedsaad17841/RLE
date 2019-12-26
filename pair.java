019d1d6396d499a798692f6abd8ff87345b8771f
mohammedSaad3030 <54086365+mohammedSaad3030@users.noreply.github.com>
2019-12-26 09:00:34 +0200

    Create pair.java

---
+++ pair.java
@@ -0,0 +1,12 @@
+class pair<T, C>
+{
+    T first;
+    C second;
+    pair(){this.first = null; this.second = null;}
+
+    pair(T first, C second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+}
